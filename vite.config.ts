0d1e78d4afbbf48ca6183af9d7a0a49ca0c66867
avefesavran-ux <av.efesavran@gmail.com>
2026-03-02 22:01:57 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -17,9 +17,7 @@ export default defineConfig(({mode}) => {
       commonjsOptions: {
         transformMixedEsModules: true,
       },
-    },
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      outDir: 'dist',
     },
     resolve: {
       alias: {
